5db3d91f7244073ffda4014ed4ac259e9e84a71f
Jemsp116 <jemspatel18082005@gmail.com>
2025-05-29 12:28:14 +0530

    :money_with_wings:

---
+++ src/components/ui/ErrorBoundary.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+}
